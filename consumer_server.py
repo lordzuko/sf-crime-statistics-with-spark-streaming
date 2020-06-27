e1e14d8def5c754b8c72b99d476897c3c7b53743
Himanshu Maurya <himanshumaurya2214225@gmail.com>
2020-06-27 23:16:26 +0530

    Added consumer server and application configuration

---
+++ consumer_server.py
@@ -0,0 +1,73 @@
+import json
+import logging
+
+from configparser import ConfigParser
+from confluent_kafka import Consumer
+from confluent_kafka import OFFSET_BEGINNING
+
+logger = logging.getLogger(__name__)
+
+class ConsumerServer:
+    """
+    Defines the bases kafka consumer class
+    """
+
+    def __init__(self, conf):
+        self.conf = conf
+        self.consumer = Consumer({
+            "bootstrap.servers": conf.get("consumer", "bootstrap.servers"),
+            "auto.offset.reset": conf.get("consumer","auto.offset.reset"),
+            "group.id": conf.get("consumer","group.id")
+        })
+
+    def on_assign(self, consumer, partitions):
+        """Callback for when topic assignment takes place"""
+        # If the topic is configured to use `offset_earliest` set the partition offset to
+        # the beginning or earliest
+        for partition in partitions:
+            if self.conf.getboolean("consumer", "offset_earliest"):
+                partition.offset = OFFSET_BEGINNING
+
+        logger.info("partitions assigned for %s", self.conf.get("consumer","group.id"))
+        consumer.assign(partitions)
+
+    def run(self):
+        """Asynchronously consumes data from kafka topic"""
+        try:
+            while True:
+                num_results = 1
+                while num_results > 0:
+                    num_results = self._consume()
+            self.close()
+        except KeyboardInterrupt as e:
+            self.close()
+            
+    def _consume(self):
+        """Polls for a message. Returns 1 if a message was received, 0 otherwise"""
+        try:
+            msg = self.consumer.poll(timeout=self.conf.get("consumer","consume_timeout"))
+            if msg:
+                self.message_handler(msg)
+                return 1
+        except Exception as e:
+            logger.error(f"Error in consumer {self.conf.get('consumer','group.id')}: {e}")
+            return 0
+
+    def message_handler(self, msg):
+        if msg is None:
+            logging.debug("No message received")
+        elif msg.error():
+            logging.error(f"Consumer error: {msg.error()}")
+        else:
+            logging.info(f"Message: {msg.value()}")
+
+    def close(self):
+        """Cleans up any open kafka consumers"""
+        self.consumer.close()
+        logger.info(f"consumer {self.conf.get('consumer','group.id')} closed!!")
+        
+if __name__ == "__main__":
+    config = ConfigParser()
+    config.read("app.cfg")
+    consumer_server = ConsumerServer(config)
+    consumer_server.run()
--- producer_server.py
+++ producer_server.py
@@ -1,8 +1,8 @@
 import json
-import time
 import logging
 
-from confluent_kafka import Producer, KafkaError
+from configparser import ConfigParser
+from confluent_kafka import Producer
 from confluent_kafka.admin import AdminClient, NewTopic
 from tqdm import tqdm
 
@@ -18,20 +18,19 @@ class ProducerServer:
     def __init__(self, conf):
         self.conf = conf
         self.producer = Producer({
-            "bootstrap.servers": conf.get("bootstrap.servers")
+            "bootstrap.servers": conf.get("producer", "bootstrap.servers")
         })
         self.client = AdminClient({
-            "bootstrap.servers": conf.get("bootstrap.servers")
+            "bootstrap.servers": conf.get("producer", "bootstrap.servers")
         })
-        if conf.get("topic_name") not in ProducerServer.existing_topics:
+        if conf.get("producer", "topic_name") not in ProducerServer.existing_topics:
             self.create_topic()
-            ProducerServer.existing_topics.add(conf.get("topic_name"))
+            ProducerServer.existing_topics.add(conf.get("producer", "topic_name"))
 
-        
-    def topic_exists(self, topic_name):
+    def topic_exists(self):
         """Checks if the given topic exists"""
         topics = self.client.list_topics(timeout=5)
-        return topics.topics.get(topic_name) is not None
+        return topics.topics.get(self.conf.get("producer", "topic_name")) is not None
 
     def create_topic(self):
         """
@@ -39,22 +38,22 @@ class ProducerServer:
         """
         exists = self.existing_topics(self.client)
         if exists:
-            logger.info(f"topic already exists: {self.topic_name}")
+            logger.info(f"topic already exists: {self.conf.get('producer', 'topic_name')}")
         else:
             futures = self.client.create_topics([
                 NewTopic(
-                    topic=self.conf.get("topic_name"),
-                    num_partitions=self.conf.get("num_partitions"),
-                    replication_factor=self.conf.get("num_replicas")
+                    topic=self.conf.get("producer", "topic_name"),
+                    num_partitions=self.conf.get("producer", "num_partitions"),
+                    replication_factor=self.conf.get("producer", "num_replicas")
                 )
             ])
 
             for topic, future in futures.items():
                 try:
                     future.result()
-                    logger.info(f"topic created: {self.conf.get('topic_name')}")
+                    logger.info(f"topic created: {self.conf.get('producer', 'topic_name')}")
                 except Exception as e:
-                    logger.error(f"failed to create topic {self.conf.get('topic_name')}: {e}")
+                    logger.error(f"failed to create topic {self.conf.get('producer', 'topic_name')}: {e}")
 
     @staticmethod
     def serialize_json(json_data):
@@ -65,19 +64,19 @@ class ProducerServer:
         """ 
         return json.dumps(json_data)
     
-    def generetate_data(self):
+    def run(self):
         """
         Read JSON data and produce serialized rows to Kafa Topic
         """
         try:
-            with open(self.conf.get('input_file')) as f:
+            with open(self.conf.get("producer","input_file")) as f:
                 data = json.loads(f.read())
-                logger.info(f"Reading {len(data)} lines from {self.conf.get('input_file')}")
+                logger.info(f"Reading {len(data)} lines from {self.conf.get('producer','input_file')}")
                 for idx, row in tqdm(enumerate(data), total=len(data), desc="Producer:> "):
                     message = self.serialize_json(row)
                     logger.info(f"Serialized Data: {message}")
                     self.producer.produce(
-                        topic=self.conf.get("topic_name"),
+                        topic=self.conf.get("producer","topic_name"),
                         value=message
                     )
             logger.info("Processing complete \n Cleaning Producer!")
@@ -89,8 +88,15 @@ class ProducerServer:
     def close(self):
         """Prepares the producer for exit by cleaning up the producer"""
         try:
-            if self.conf.get("topic_name"):
+            if self.conf.get("producer","topic_name"):
                 self.producer.flush()
                 logger.info("Producer Shutdown!!! ")
         except Exception as e:
             logger.error("producer close incomplete - skipping")
+            
+if __name__ == "__main__":
+    config = ConfigParser()
+    config.read("app.cfg")
+    producer_server = ProducerServer(config)
+    producer_server.run()
+    
